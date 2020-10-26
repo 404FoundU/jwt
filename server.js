2247044f5cb1de4a43e18d27dc0f5eae28c5151d
upillai <upillai@xpressdocs.com>
2020-10-26 12:01:09 -0500

    jwt project

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+const posts = [
+    {
+        username: "Unni1",
+        title: "post1"
+    },
+    {
+        username: "Unni2",
+        title: "post2"
+    }
+];
+app.get('/posts',authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name));
+});
+app.post('/login', (req, res) => {
+    // Auth user
+    const username = req.body.username;
+    const user = {name: username};
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({accessToken: accessToken})
+});
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+
+}
+app.listen(3000);
